3a5a955ae4e48c45e51ed0c1e364cd38a619ad2f
Manuel <m.moracuesta@gmail.com>
2014-12-06 23:54:58 +0100

    Fixed rope prefab path

--- Assets/Scripts/Items/SceneItems/RopeSceneItem.cs
+++ Assets/Scripts/Items/SceneItems/RopeSceneItem.cs
@@ -5,7 +5,7 @@ public class RopeSceneItem : SceneItem {
 
 	public override void Use (InventoryItem item)
 	{
-		Object prefab = Resources.Load("Prefabs/RopeInventary");
+		Object prefab = Resources.Load("Prefabs/Rope");
 		GameObject rope = Instantiate(prefab) as GameObject;
 		GameManager.GetInstance().GetComponent<Inventory>().AddItem(rope.GetComponent<InventoryItem>());
 	}

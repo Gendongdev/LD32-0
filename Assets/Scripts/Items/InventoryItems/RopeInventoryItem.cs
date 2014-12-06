e3993c9535d0369ff093094603cfabee0d71e14d
Manuel <m.moracuesta@gmail.com>
2014-12-06 22:20:59 +0100

    Added prefabs for Octorope, Rope and Octopus item.

--- Assets/Scripts/Items/InventoryItems/RopeInventoryItem.cs
+++ Assets/Scripts/Items/InventoryItems/RopeInventoryItem.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class RopeInventoryItem : InventoryItem
 {
-	public OctoropeInventoryItem m_octoropeItem;
-
 	public override void Use (InventoryItem item)
 	{
 		if(item is OctopusInventoryItem)
 		{
 			GameManager.GetInstance().GetComponent<Inventory>().RemoveItem(item);
 			GameManager.GetInstance().GetComponent<Inventory>().RemoveItem(this);
-			GameManager.GetInstance().GetComponent<Inventory>().AddItem(m_octoropeItem);
+			GameObject prefab = Resources.Load("Prefabs/Octorope");
+			GameObject octorope = Instantiate(prefab);
+			GameManager.GetInstance().GetComponent<Inventory>().AddItem(octorope);
 			return;
 		}
 

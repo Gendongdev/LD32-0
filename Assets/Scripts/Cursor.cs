e0405dbaef19160719d43e5df9c3b951a7a6d1bf
isijose2 <isijose2@gmail.com>
2014-12-07 13:40:39 +0100

    Cursor move and interact.

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -3,24 +3,46 @@ using System.Collections;
 
 public class Cursor : MonoBehaviour
 {
-	public InventoryItem m_item;
+    public InventoryItem m_item;
+    private PlayerController _player;
 
-	public InventoryItem Item
-	{
-		get { return m_item; }
-		set
-		{
-			m_item = value;
-		}
-	}
+    public InventoryItem Item
+    {
+        get { return m_item; }
+        set
+        {
+            m_item = value;
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        _player = GameObject.Find("Guy").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, float.PositiveInfinity, LayerMask.GetMask("InteractiveObject"));
+            if (hit != null && hit.transform != null)
+            {
+                _player.Move(hit.transform.gameObject.GetComponent<SceneItem>(), Input.GetMouseButtonDown(1) || Item != null);
+            }
+            else
+            {
+                hit = Physics2D.Raycast(ray.origin, ray.direction, float.PositiveInfinity, LayerMask.GetMask("Floor"));
+
+                if (hit != null && hit.transform != null)
+                {
+                    _player.Move(null, false);
+                }
+            }
+        }
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,46 +5,69 @@ public class PlayerController : MonoBehaviour {
 
 	Vector3 m_positionToGo;
 	public float m_speed;
+    private Animator _myAnim;
+    private SceneItem _useItem = null;
+    private Cursor _cursorManager;
+    private bool _useObject = false;
 
 	// Use this for initialization
 	void Start () {
 		m_positionToGo = Vector3.zero;
+        _myAnim = GetComponent<Animator>();
+        _cursorManager = GameManager.GetInstance().GetComponent<Cursor>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		CheckInput ();
+        if (Mathf.Abs(m_positionToGo.x - transform.position.x) > 0.5f)
+        {
+            Vector3 dir = Vector3.zero;
+            dir.x = m_positionToGo.x - transform.position.x;
+            dir.Normalize();
+            Debug.Log("Direction: " + dir);
+            transform.position += dir * m_speed * Time.deltaTime;
+        }
+        else
+        {
+            _myAnim.SetBool("StartWalk", false);
+            _myAnim.SetBool("EndWalk", true);
 
-		if (Mathf.Abs(m_positionToGo.x - transform.position.x) > 0.5f) 
-		{
-			Vector3 dir = Vector3.zero;
-			dir.x = m_positionToGo.x - transform.position.x;
-			dir.Normalize();
-			Debug.Log("Direction: "+dir);
-			transform.position += dir * m_speed *Time.deltaTime;
-		}
+            if (_useItem != null)
+            {
+                if (_useObject)
+                {
+                    _useItem.Use(_cursorManager.Item);
+                    _cursorManager.Item = null;
+                }
+                else
+                    Debug.Log("Look");
+                        
+            }
+
+            enabled = false;
+        }
 
 	}
 
-	void MoveTo(Vector3 pos)
+	public void Move(SceneItem item, bool use)
 	{
+        Vector3 scale = transform.localScale;
+        _useObject = use;
+        _useItem = item;
 		m_positionToGo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        _myAnim.SetBool("StartWalk",true);
+        _myAnim.SetBool("EndWalk", false);
+
+        if (m_positionToGo.x < transform.position.x)
+            scale.x = 1;
+        else
+            scale.x = -1;
+
+        transform.localScale = scale;
+
+        enabled = true;
 	}
 
-	void CheckInput()
-	{
-		if (Input.GetMouseButtonDown (0)) {
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit))
-			{
-				//if (hit.collider.GetComponent<
-			}
-		}
-		if(Input.GetMouseButtonDown(1))
-			Debug.Log("Pressed right click.");
-		if(Input.GetMouseButtonDown(2))
-			Debug.Log("Pressed middle click.");
-	}
 }

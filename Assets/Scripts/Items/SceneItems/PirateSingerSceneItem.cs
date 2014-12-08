382808896ff6606de3258117767217df481c9851
Guillermo Gijón <guillermogijon@nwink.net>
2014-12-08 17:44:38 +0100

    Fixed singing pirate so you can't give him the cat

---
+++ Assets/Scripts/Items/SceneItems/PirateSingerSceneItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PirateSingerSceneItem : SceneItem
+{
+    public override void Use(InventoryItem item)
+    {
+        if (item == null)
+        {
+            MessageServer.SendMessage("song_text", Color.cyan);
+        }
+        else
+        {
+            base.Use(item);
+        }
+    }
+}
